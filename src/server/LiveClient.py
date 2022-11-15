19b691e674fbee18f19b8d84926735534033222c
Jonathan Sigrist <jonathan.sigrist@xitaso.com>
2022-11-16 00:29:58 +0100

    liveclient communicating with server

---
+++ src/server/LiveClient.py
@@ -0,0 +1,59 @@
+from argparse import ArgumentError
+import grpc
+from protos.message_board.message_board_pb2 import Cookie, Credentials, Post, ReadPost, WritePost
+from protos.message_board.message_board_pb2_grpc import MessageBoardStub
+
+
+class LiveClient:
+
+    def __init__(self):
+        channel = grpc.insecure_channel('localhost:50051')
+        self.stub = MessageBoardStub(channel)
+
+    def register(self, username: str, password: str):
+        creds = Credentials(username=str(username), password=str(password))
+        self.stub.register(creds)
+
+    def login(self, username: str, password: str):
+        creds = Credentials(username=str(username), password=str(password))
+        resp: Cookie = self.stub.login(creds)
+        return resp.cookie
+
+    def logout(self, cookie: str):
+        cookie = Cookie(cookie=cookie)
+        self.stub.logout(cookie)
+
+    def write(self, cookie: str, text: str):
+        cookie = Cookie(cookie = cookie)
+        post = Post(text = text)
+        writePost = WritePost(cookie = cookie, post = post)
+        self.stub.write(writePost)
+
+    def read_all(self, cookie: str):
+        cookie = Cookie(cookie = cookie)
+        for resp in self.stub.read_all(cookie):
+            yield resp.text
+
+    def read(self, cookie: str, index: int):
+        cookie = Cookie(cookie = cookie)
+        readPost = ReadPost(cookie = cookie, index = index)
+        resp: Post = self.stub.read(readPost)
+        return resp.text
+
+    def interactive(self):
+        while cmd := input("cmd: "):
+            print(eval(cmd))
+
+if __name__ == "__main__":
+    c = LiveClient()
+    c.register("Hello", "World")
+    cookie: str = c.login("Hello", "World")
+    print(f"{cookie=}")
+    c.write(cookie, "Hello")
+    c.write(cookie, "World")
+    print(list(c.read_all(cookie)))
+    try:
+        c.read(cookie, 10)
+    except grpc.RpcError as rpc_error:
+        print(rpc_error)
+    
--- src/server/LiveServer.py
+++ src/server/LiveServer.py
@@ -1,8 +1,10 @@
 from concurrent.futures import ThreadPoolExecutor
+from typing import Generator
 from uuid import uuid4
 import grpc
 from protos.message_board.message_board_pb2_grpc import MessageBoardServicer, add_MessageBoardServicer_to_server
 from protos.message_board.message_board_pb2 import Cookie, Credentials, Post, PostAmount, ReadPost, WritePost
+from google.protobuf.empty_pb2 import Empty
 from Exceptions import WrongCredentials
 
 class MessageBoardImpl(MessageBoardServicer):
@@ -16,8 +18,9 @@ class MessageBoardImpl(MessageBoardServicer):
     def register(self, request: Credentials, context) -> Cookie:
         if request.username is None: raise WrongCredentials("Please enter a username!")
         if request.password is None: raise WrongCredentials("Please enter a password!")
-        self.passwords[request.username] = self.password # exploit by setting new password for existing user
+        self.passwords[request.username] = request.password # exploit by setting new password for existing user
         self.messages[request.username] = []
+        return Empty()
 
     def login(self, request: Credentials, context):
         if request.username is None: raise WrongCredentials("Please enter a username!")
@@ -26,25 +29,33 @@ class MessageBoardImpl(MessageBoardServicer):
         if self.passwords[request.username] != request.password: raise WrongCredentials("Password is wrong!")
         cookie = uuid4().hex
         self.active[cookie] = request.username
-        return Cookie(cookie)
+        return Cookie(cookie=cookie)
 
     def logout(self, request: Cookie, context):
         if request.cookie is None: raise WrongCredentials("Please enter a cookie!")
         if request.cookie not in self.active: raise WrongCredentials("Cookie is not authenticated!")
         del self.active[request.cookie]
+        return Empty()
 
     def get_posts(self, request: Cookie, context) -> PostAmount:
         if request.cookie is None: raise WrongCredentials("Please enter a cookie!")
         if request.cookie not in self.active: raise WrongCredentials("Cookie is not authenticated!")
         username = self.active[request.cookie]
-        return PostAmount(len(self.messages[username]))
+        return PostAmount(amount=len(self.messages[username]))
 
     def read(self, request: ReadPost, context) -> Post:
+        if request.cookie is None or (cookie := request.cookie.cookie) is None: raise WrongCredentials("Please enter a cookie!")
+        if cookie not in self.active: raise WrongCredentials("Cookie is not authenticated!")
+        username = self.active[cookie]
+        index = request.index or 0
+        return Post(text=self.messages[username][index])
+
+    def read_all(self, request: Cookie, context) -> Generator[Post, None, None]:
         if request.cookie is None: raise WrongCredentials("Please enter a cookie!")
         if request.cookie not in self.active: raise WrongCredentials("Cookie is not authenticated!")
         username = self.active[request.cookie]
-        index = request.index or 0
-        return Post(self.messages[request.cookie][index])
+        for msg in self.messages[username]:
+            yield Post(text = msg)
         
     def write(self, request: WritePost, context):
         if request.cookie is None or (cookie := request.cookie.cookie) is None: raise WrongCredentials("Please enter a cookie!")
@@ -52,7 +63,8 @@ class MessageBoardImpl(MessageBoardServicer):
         if request.post is None or (text := request.post.text) is None: raise AttributeError("Please enter a text!")
         username = self.active[cookie]
         if len(self.messages[username]) >= 50: raise AttributeError("Can not post any more messages with this account!")
-        self.messages.append(text)
+        self.messages[username].append(text)
+        return Empty()
     
 def serve():
     server = grpc.server(ThreadPoolExecutor(max_workers=10))
@@ -64,4 +76,3 @@ def serve():
 if __name__ == "__main__":
     print("Starting server...")
     serve()
-    print("Started server...")
